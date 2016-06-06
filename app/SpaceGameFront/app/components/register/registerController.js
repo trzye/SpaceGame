744ad7f5f78256f615c05da156eb882744a4327a
Dariusz Dybka <dybka.dariusz@gmail.com>
2016-06-06 18:54:31 +0200

    dodalem podstawową logike registerController

--- app/SpaceGameFront/app/components/register/registerController.js
+++ app/SpaceGameFront/app/components/register/registerController.js
@@ -1,5 +1,5 @@
 angular.module("SpaceGame.RegisterModule", [])
-    .controller("RegisterController", ["$scope", function ($scope) {
+    .controller("RegisterController", ["$scope", "$http", function ($scope, $http) {
 
         $.backstretch([
             "resources/img/bg-login-1.jpg",
@@ -9,10 +9,54 @@ angular.module("SpaceGame.RegisterModule", [])
             duration: 7000
         });
 
+        $scope.username = "";
+        $scope.password = "";
+        $scope.passwordRepeat = "";
+        $scope.email = "";
+        $scope.error = [];
+
         $scope.selectedPlanet = {
             "name": "nie wybrano",
             "x": -1,
             "y": -1
+        };
+
+        $scope.register = function() {
+
+            $scope.error = [];
+
+            if ($scope.password.length < 3) {
+                $scope.error.push("Hasło powinno być dłuższe niż 3 znaki!")
+            }
+
+            if ($scope.password != $scope.passwordRepeat) {
+                $scope.error.push("Hasła nie są takie same");
+            }
+
+            if ($scope.username.length < 3) {
+                $scope.error.push("Nazwa użytkownika powinna mieć więzej niż 3 zanki!");
+            }
+
+            if ($scope.error.length == 0) {
+                var data = {
+                    "nickname" : $scope.username,
+                    "rawPassword": $scope.password,
+                    "email": $scope.email,
+                    "coordinateX" : 0,
+                    "coordinateY" : 0
+                };
+
+                $http({
+                    method: "POST",
+                    url: "http://localhost:8080/signUp",
+                    data: JSON.stringify(data),
+                    headers: {"Content-Type": "application/json"}
+                }).then(function success(response){
+
+                }, function error(response) {
+
+                });
+            }
         }
         
 
